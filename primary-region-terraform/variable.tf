16e54a36ff8352d4d61fdbe12ac81a40b3883e8f
Smitha-CN <smithamalthi@gmail.com>
2025-09-03 09:59:41 -0400

    Update variable.tf

--- primary-region-terraform/variable.tf
+++ primary-region-terraform/variable.tf
@@ -1,29 +1,20 @@
-variable "vpc_cidr"           
-{ 
-default ="10.0.0.0/16"
-}
-variable "public_subnet_1"   
-{ 
-default = "10.0.1.0/24" 
-}
-variable "public_subnet_2"    
-{
-default ="10.0.2.0/24"
-}
-variable "private_subnet_1"   
-{
-default = "10.0.3.0/24" 
-}
-variable "private_subnet_2"   
-{ 
-default = "10.0.4.0/24" 
+variable "region" {
+  description = "AWS region where resources will be deployed"
+  default     = "us-east-1"
 }
 
-variable "db_username"    
-{ 
-default = "admin" 
+variable "db_username" {
+  description = "Database username"
+  default     = "admin"
 }
-variable "db_password"    
-{ 
-default = "admin1234"
+
+variable "db_password" {
+  description = "Database password"
+  default     = "adminadmin"
+  sensitive   = true
+}
+
+variable "eks_admin_iam_arn" {
+  description = "IAM ARN for EKS admin user"
+  default     = "arn:aws:iam::047719630685:user/Admin-USer"
 }
